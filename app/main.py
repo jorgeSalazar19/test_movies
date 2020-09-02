828fe27c69f1bff8bc8e2bc26090056cb9565b25
Leo López <lflorez@ikatechsolutions.com>
2020-09-01 20:22:04 -0500

    add class sort to sort json

--- app/main.py
+++ app/main.py
@@ -6,29 +6,36 @@ class Movie(SortMovies, ManagerData):
 
     def __init__(self, path_movies):
         self._path_movies = path_movies
-        self._data_movies = None
         # call method for upload movies
-        self.upload_movies()
+        self._upload_movies()
     
 
     @property
     def data_movies(self):
         return self._data_movies
+
+    
+    @data_movies.setter
+    def data_movies(self, data):
+        self._data_movies = data
     
 
-    def upload_movies(self):
+    def _upload_movies(self):
         self._data_movies = self.upload(self._path_movies)
-        self.calculated_average_rating(self.data_movies)
+        self.calculated_average_rating(self._data_movies)
     
 
-    def sort_movies(self):
-        self.sort_to_average(self._data_movies)
+    def sort_movies(self, option_sort):
+        self.sort_to_average([2, 4 ,2 ,5 ,4, 3])
 
 
 
 # funtion main for the test sort and upload files
 if __name__ == "__main__":
 
+    print("Ingrese la forma de ordenar el archivo: ")
+    option_sort = int(input())
+
     # upload data and calculate averga rating for each movie into the json
     movies = Movie('movies.json')
-    movies.sort_movies()
+    movies.sort_movies(option_sort)
